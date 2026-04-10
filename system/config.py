ac3a00bcae6b7d9c02b686758e79ff126a554b6a
soIver <sasha.radygin@yandex.ru>
2026-04-11 01:43:35 +0300

    configuration updated

---
+++ system/config.py
@@ -0,0 +1,13 @@
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+class Config():
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = int(os.getenv("DB_PORT"))
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+config = Config()
